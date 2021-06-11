faf8906bc3fda28158f36da2b75b7b372065a2cc
million23 <67777757+million23@users.noreply.github.com>
2021-06-11 12:27:12 +0800

    Transaction History Completed

--- WearAndGo_Admin/Views/Dashboard/TransactionHistory.xaml.cs
+++ WearAndGo_Admin/Views/Dashboard/TransactionHistory.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using ModernWpf.Controls;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -21,7 +22,7 @@ namespace WearAndGo_Admin.Views.Dashboard
     /// <summary>
     /// Interaction logic for TransactionHistory.xaml
     /// </summary>
-    public partial class TransactionHistory : Page
+    public partial class TransactionHistory : System.Windows.Controls.Page
     {
         public TransactionHistory()
         {
@@ -52,6 +53,93 @@ namespace WearAndGo_Admin.Views.Dashboard
             }
 
             MainGrid.ItemsSource = thisTable.DefaultView;
+        }
+
+        private void RefreshList(object sender, RoutedEventArgs e)
+        {
+            GetData(null, null);
+        }
+
+        private void FindInList(object sender, RoutedEventArgs e)
+        {
+            splitView.IsPaneOpen = true;
+        }
+
+        private async void SearchFromList(object sender, RoutedEventArgs e)
+        {
+            bool arefound = false;
+            splitView.IsPaneOpen = false;
+
+            if (search_transactionID.Text == string.Empty && search_owner.Text == string.Empty)
+            {
+                var dialog = new ContentDialog
+                {
+                    Title = "Search Error",
+                    Content = "Please fill in one of the search textbox",
+                    CloseButtonText = "Ok",
+                    DefaultButton = ContentDialogButton.Close
+                };
+                await dialog.ShowAsync();
+            } else
+            {
+
+                string MyDocumentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                var fromFile = JObject.Parse(File.ReadAllText($"{MyDocumentsPath}/WearAndGoS2/TransactionHistory.json"));
+
+
+                var thisTable = new DataTable();
+                thisTable.Columns.Add("Transaction ID");
+                thisTable.Columns.Add("Owner");
+                thisTable.Columns.Add("Date Purchased");
+                thisTable.Columns.Add("Total Purchase");
+
+
+                foreach (var item in fromFile["History"])
+                {
+                    if ((string)item["TransactionID"] == search_transactionID.Text)
+                    {
+                        var row = thisTable.NewRow();
+                        row["Transaction ID"] = item["TransactionID"];
+                        row["Owner"] = item["User"];
+                        row["Date Purchased"] = item["Date"];
+                        row["Total Purchase"] = item["Total"];
+
+                        thisTable.Rows.Add(row);
+                        arefound = true;
+
+                    }else if ((string)item["User"] == search_owner.Text)
+                    {
+                        var row = thisTable.NewRow();
+                        row["Transaction ID"] = item["TransactionID"];
+                        row["Owner"] = item["User"];
+                        row["Date Purchased"] = item["Date"];
+                        row["Total Purchase"] = item["Total"];
+
+                        thisTable.Rows.Add(row);
+                        arefound = true;
+                    }
+                }
+
+                if (!arefound)
+                {
+                    var dialog = new ContentDialog
+                    {
+                        Title = "Unidentified Object",
+                        Content = "We cannot find that information. Try again.",
+                        CloseButtonText = "Ok",
+                        DefaultButton = ContentDialogButton.Close
+                    };
+                    await dialog.ShowAsync();
+                } else
+                {
+                    MainGrid.ItemsSource = thisTable.DefaultView;
+                }
+
+                search_owner.Text = string.Empty;
+                search_transactionID.Text = string.Empty;
+
+            }
+
         }
     }
 }
